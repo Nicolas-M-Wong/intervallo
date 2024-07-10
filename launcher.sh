01085618fdcce375efe3615c25f2afa4a89822d0
Nicolas <69300323+Nicolas-M-Wong@users.noreply.github.com>
2024-07-10 22:01:37 +1100

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -19,8 +19,8 @@ if [ $? -ne 0 ]; then
     echo "No internet connection."
     exit 1
 else
-git init
-git pull https://www.github.com/Nicolas-M-Wong/intervallo main
+    git init
+    git pull https://www.github.com/Nicolas-M-Wong/intervallo main
 fi
 
 python3 intervallo-server-1.py
