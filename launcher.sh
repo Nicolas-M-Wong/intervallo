49e429e6bdfde7a9064b30d9390337d345cd5c96
Nicolas <69300323+Nicolas-M-Wong@users.noreply.github.com>
2024-07-20 02:41:15 +0200

    Update launcher.sh
    improve logging readability and remove git warning

--- launcher.sh
+++ launcher.sh
@@ -1,6 +1,24 @@
-#!/bin/bash
-sleep 10
+center_text() {
+    local msg="$1"
+    local character="$2"
+    if [ -t 1 ]; then
+        # Inside a terminal, use `tput` to get terminal width
+        width=$(tput cols)
+    else
+        # Default to 80 columns if not inside a terminal
+        width=80
+    fi
+    LENGTH_MSG=${#msg}
+    CENTER_COL=$(((width - LENGTH_MSG) / 2))
+    # Repeat the character COUNT times
+    printf '%*s\n' "$width" '' | tr ' ' "$character"
+    printf "%*s%s\n" $CENTER_COL "" "$msg"
+    printf "%*s\n" "$width" | tr ' ' "$character"
+    }
 
+center_text "$(date)" "-"
+
+sleep 10
 
 DESKTOP_DIR=$(xdg-user-dir DESKTOP)
 #find the user desktop directory
@@ -11,4 +29,9 @@ cd main
 #go to main
 
 git init
+git config pull.rebase false
 git pull https://www.github.com/Nicolas-M-Wong/intervallo main
+
+python3 intervallo-server-1.py
+
+center_text "fin" "-"
