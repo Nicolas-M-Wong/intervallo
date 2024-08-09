e75aaaf287ca7aa16ab2b7f84373ca1cd5a9b5e5
Nicolas <69300323+Nicolas-M-Wong@users.noreply.github.com>
2024-08-09 11:12:33 +0200

    Update js-home.js

--- src/js-home.js
+++ src/js-home.js
@@ -1,133 +1,354 @@
-<!DOCTYPE html>
-<html>
-<head>
-<link rel="stylesheet" href="css-home.css">
-<link rel="alternate" href="home-big-screen.html" id="big-screen">
-<link rel="alternate" href="home-landscape-screen.html" id="landscape-screen">
+// Dialog Box
+let dialogBoxId = document.getElementById("dialogBox");
+let countdownInterval;
+let countDownDate;
 
-<meta name="viewport" content="width=device-width, initial-scale=1.0", charset="UTF-8">
-
-<title>Intervallomètre</title>
-
-</head>
-
-<body id="screen-container" data-mode="dark">
-
-<div id="phone-screen" class="phone-screen-class">
-<div class="container" id="main" data-mode="dark">
-
-<div id="header-bar" class="header-bar" data-mode="dark">
-	<div id="left-side" class="flex-column-title">
+function showDialog(nbPhotos, exposureTime, timeBetweenPhotos) {
+	const notificationMessage = document.getElementById("notificationMessage");
+	const notificationTitle = document.getElementById("notificationTitle");
 	
-	</div>
-	<div id="right-side" class="flex-column-title">
-		<a id="toggle-mode">
-		<div class="circle" id="half-circle" data-mode="dark"></div>
-		</a>
+    dialogBoxId.addEventListener("keydown", (e) => {
+        if (e.key === "Escape") {
+            e.preventDefault();
+        }
+    });
+    document.getElementById("Compteur").innerHTML = "00:00:00"; // Initialize countdown display
+    dialogBoxId.showModal();
+    
+    // Calculate the total time
 
-		<a class="checkbox-round-sleep" value="sleep" onclick="serverEnd('sleep')">
-		<div class="circle" id="full-circle-sleep"></div>
-		</a>
-
-		<a class="checkbox-round-shutdown" value="shutdown" onclick="serverEnd('shutdown')">
-		<div class="circle" id="full-circle-shutdown"></div>
-		</a>
-		</div>
-	</div>
-<div id="notification" data-mode="hide">
-	<div class="txt-notif"><span class="notification-title" id="notificationTitle">La série se termine à X</span>
-	</br><span class="notification-message" id="notificationMessage">X photos prises, exposition X sec.</span>
-	</div>
-    <div id="close-notif">
-		<a id="toggle-notif">&#10006;</a>
-	</div>
-</div>
-
-	<div class="IntervalForm" id="interval-Form">
-	
-        	<div class="flex-container-title">
-            	<div class="flex-column-title">
-        			<h3 class="Title-3">Paramètres</h3>
-        		</div>
-    		</div>
-    		
-    		<form method="post" class="param-submit">
-    		
-        		<div class="param-submit">
-				
-					<div class="label-info">
-						<label for="nb_photo">Nombre de photo</label>
-						<span class="tooltip" title="Nombre de photo qui seront prise par l'appareil">&#x24D8</span>
-            		</div>
-					<input type="number" name="nb_photo" id ="nb_photo" data-mode="dark" required />
-					
-					<div class="label-info">
-						<label for="tmp_pose">Temps d'expo</label>
-						<span class="tooltip" title="Temps d'exposition pour une photo">&#x24D8</span>
-            		</div>
-            		<input type="number" step="0.01" name="tmp_pose" id ="tmp_pose" data-mode="dark" required />
-					
-					<div class="label-info">
-						<label for="enregistrement">Intervalle</label>
-						<span class="tooltip" title="Intervalle de temps entre chaque photo">&#x24D8</span>
-            		</div>
-            		<input type="number" step="0.01" name="tmp_enregistrement" id ="enregistrement" data-mode="dark"required />
+    const totalTime = nbPhotos * exposureTime + timeBetweenPhotos * (nbPhotos - 1);
+    
+     // Set countdown date to current time plus total time
      
-            		<input type="submit" id="form-submit" value="Ok"/>
-                </div>
-            </form>  
-    	</div>
-    
-        <div id="confirmation" style="display: none;">
-            <h3>Temps de prise de vue estimé : </h3><h3 id="estimation_tmp"></h3>
-            <form action = "" method="post" class = "confirmation-form">
-                <div class="flex-column-confirm">
-                    <button name="confirm" value="Confirmer" id="openDialogBox">Confirmer</button>
-                    <button name="test-shot" value="Photo_test" id="test-shot">Photo test</button>
-                </div>
-            </form>
-        </div>
-    
-        <dialog id="dialogBox" data-mode="dark">
-            <h3>Temps restant :‎</h3>
-            <h3 id="Compteur"></h3>
-            <footer>
-            <div class="back-button">
-                <button id="back-button" onclick="handleButtonClickBack(event)" type="button">
-                Retour </button>
-            </div>
-            </footer>
-        </dialog>
-		<div class="navbar-container">
-			<nav class="navbar" id="navbar-id" data-mode="dark">
-				<a name="photo-distance" id="photo-distance-L" onclick="remoteTrigger();changeColor('L');navigator.vibrate([250]);"><img src="ico-camera.ico" width="32" height="32" alt="Déclencheur"></a>
-				<a href="" name="intervallo-home" id="intervallo-home"><img width="32" height="32" src="ico-intervallo.ico" ></a>
-				<a name="photo-distance" id="photo-distance-R" onclick="remoteTrigger();changeColor('R');navigator.vibrate([250]);"><img src="ico-camera.ico" width="32" height="32" alt="Déclencheur"></a>
-			</nav>
-		</div>
-    </div>
-    
-    <div id="shutdown" class="shutdown-class" data-mode="dark">
-		<div id="shutdown-inf">
-        <div class="shutdown-wrapper" id="shutdown-wrapper" data-mode="dark">
-            <div class="shutdown-content" data-mode="dark">
-                <h1 class="shutdown-title">Hop au dodo</h1>
-                <p class="shutdown-message">L'intervallomètre va s'éteindre dans 15 sec</p>
-            </div>
-            <div class="loading-wrapper">
-                <div class="rotating-object"></div>
-            </div>
-        </div>
-		</div>
-    </div>
-	</div>
+    const now = new Date().getTime();
+    const countDownDate = now + totalTime * 1000;
+
+    // Convert countdown date to readable format
+    const countDownDateObj = new Date(countDownDate);
+    const hours = countDownDateObj.getHours().toString().padStart(2, '0');
+    const minutes = countDownDateObj.getMinutes().toString().padStart(2, '0');
+    const currentTime = `${hours}:${minutes}`;
+    sessionStorage.nbPhotosNotif = nbPhotos;
+    sessionStorage.exposureTimeNotif = exposureTime;
+    sessionStorage.currentTimeNotif = currentTime;
+
+	toggleNotif();
+    // Start the countdown
+    countdownInterval = setInterval(function() {
+        var now = new Date().getTime();
+        var distance = (countDownDate - now);
+        document.getElementById("Compteur").innerHTML = formatTime(distance / 1000);
+        if (distance < 0) {
+            clearInterval(countdownInterval);
+            document.getElementById("Compteur").innerHTML = "00:00:00";
+			navigator.vibrate([1000, 200, 1000]); // vibrate for 200ms
+        }
+    }, 1000);
+}
+
+function closeDialog() {
+    dialogBoxId.close(); 
+    // Clear the countdown interval when the dialog box is closed
+    clearInterval(countdownInterval);
+}
+
+// Function to format time in hh:mm:ss
+function formatTime(totalSeconds) {
+    const hours = Math.floor(totalSeconds / 3600);
+    const minutes = Math.floor((totalSeconds % 3600) / 60);
+    const seconds = Math.floor(totalSeconds % 60);
+    return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+}
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------
+
+// Add event listener to the form submit event
+document.getElementById("interval-Form").addEventListener("submit", handleSubmit);
+document.getElementById("openDialogBox").addEventListener("click", handleButtonClick);
+document.getElementById("test-shot").addEventListener("click", handleButtonClickTest);
+
+let formData; // Define formData in the global scope
+
+function handleSubmit(event) {
+	// Prevent the default form submission behavior
+	event.preventDefault();
+
+	// Get the values of the form fields
+	const nbPhotos = parseInt(document.getElementById("nb_photo").value);
+	const exposureTime = parseFloat(document.getElementById("tmp_pose").value);
+	const timeBetweenPhotos = parseFloat(document.getElementById("enregistrement").value);
+
+	// Calculate the total time
+	const totalTime = nbPhotos * exposureTime + timeBetweenPhotos * (nbPhotos - 1);
+	console.log("Total time for the interval:", totalTime, "seconds");
+
+	// Display formatted time in confirmation
+	document.getElementById("estimation_tmp").innerHTML = formatTime(totalTime);
+	document.getElementById("confirmation").style.display = "block";
+
+	// Prepare form data for the POST request
+	formData = new FormData(event.target);
+}
+
+function handleButtonClick() {
+    if (formData) {
+        const data = {};
+        formData.forEach((value, key) => (data[key] = value));
+		var now = new Date().getTime();
+		data["date"] = now;
+        sendPostRequest(data);
 		
-</div>
+		const nbPhotos_button = parseInt(document.getElementById("nb_photo").value);
+		const exposureTime_button = parseFloat(document.getElementById("tmp_pose").value);
+		const timeBetweenPhotos_button = parseFloat(document.getElementById("enregistrement").value);
+		
+        showDialog(nbPhotos_button, exposureTime_button, timeBetweenPhotos_button); // Show the dialog box with the countdown
+    } else {
+        console.error('Form data is not available. Please submit the form first.');
+    }
+}
 
-</div>
+function handleButtonClickTest() {
+    if (formData) {
+        const data = {};
+        formData.forEach((value, key) => (data[key] = value));
+		data['nb_photo']=1;
+		var now = new Date().getTime();
+		data["date"] = now;
+        sendPostRequest(data);
+				
+		const exposureTime_test = parseFloat(document.getElementById("tmp_pose").value);
+		const timeBetweenPhotos_test = parseFloat(document.getElementById("enregistrement").value);
+		
+		showDialog(1, exposureTime_test, timeBetweenPhotos_test)
+        } 
+	else {
+        console.error('Form data is not available. Please submit the form first.');
+    }
+}
 
-<!-- ------------------------------------------------------------------------------- -->
+function remoteTrigger(){
+const triggerMessage = {"nb_photo":"1", "tmp_pose":"0.1", "tmp_enregistrement":"0"};
+var now = new Date().getTime();
+triggerMessage["date"] = now;
+console.log(triggerMessage);
+sendPostRequest(triggerMessage)
+}
 
-<script src="js-home.js"></script>
-</body>
-</html>
+function sendPostRequest(data) {
+    const xhr = new XMLHttpRequest();
+    var ip = location.host;
+    var http_head = 'http://'
+    xhr.open('POST', http_head.concat(ip), true);
+    xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                console.log('Success:', xhr.responseText);
+            } else {
+                console.error('Error:', xhr.statusText);
+            }
+        }
+    };
+
+    xhr.send(JSON.stringify(data));
+}
+
+// Prevent the dialog box from closing when the confirm button is clicked
+document.getElementById("confirmation").addEventListener("click", function(event) {
+    event.preventDefault();
+
+    // Add any additional actions you want to perform on confirm
+});
+
+function DetectDevice() {
+    let isMobile = window.matchMedia || window.msMatchMedia;
+    if(isMobile) {
+        let match_mobile = isMobile("(pointer:coarse)");
+        return match_mobile.matches;
+    }
+    return false;
+}
+
+function sendGetRequest(fileName) {
+    const url = `/${fileName}`;
+
+    fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'text/plain', // Adjust the content type if necessary
+        }
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.text(); // You can change this to response.json() if the response is JSON
+    })
+    .then(data => {
+	document.getElementById('screen-container').innerHTML = data;
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
+}
+
+function detectLandscapeOrientation() {
+    const orientation = window.matchMedia("(orientation: landscape)").matches;
+    const wideScreen= window.innerWidth > 800;
+    return orientation||wideScreen;
+}
+
+function toggleNotif(){
+	// Update notification message and title
+	var locPhotos = sessionStorage.getItem("nbPhotosNotif");
+	var locExpo = sessionStorage.getItem("exposureTimeNotif");
+	var locTime = sessionStorage.getItem("currentTimeNotif");
+
+	notificationMessage.textContent = `${locPhotos} photos prises, exposition ${locExpo}.`;
+	notificationTitle.textContent = `Session se termine à ${locTime}`;
+
+	const elementsToToggle = [document.getElementById('notification')];
+	elementsToToggle.forEach(element => {
+		sessionStorage.setItem('notifState', 'show');
+		element.dataset.mode = 'show';
+	});
+}
+
+const phone = DetectDevice()
+
+if (phone == false) {
+	document.getElementById('phone-screen').style.display = "none";
+	sendGetRequest(document.getElementById('big-screen').getAttribute('href'));
+}
+
+else if (detectLandscapeOrientation()) {
+	document.getElementById('phone-screen').style.display = "none";
+	sendGetRequest(document.getElementById('landscape-screen').getAttribute('href'));
+}
+
+function handleButtonClickBack(event) {
+	event.preventDefault();
+	closeDialog();
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleBtn = document.getElementById('toggle-mode');
+    const body = document.body;
+    const elementsToToggle = [body, document.getElementById('dialogBox'),
+	document.getElementById('main'),document.getElementById('navbar-id'),
+	document.getElementById('nb_photo'),document.getElementById('tmp_pose'),
+	document.getElementById('enregistrement'),document.getElementById('shutdown'),
+	document.getElementById('big-screen'),document.getElementById('landscape-screen'),
+	document.getElementById('shutdown-wrapper'),document.getElementById('half-circle')];
+
+    const currentTheme = sessionStorage.getItem('theme') || 'dark';
+    elementsToToggle.forEach(element => {
+        element.dataset.mode = currentTheme;
+    });
+
+    toggleBtn.addEventListener('click', () => {
+        const newMode = body.dataset.mode === 'dark' ? 'light' : 'dark';
+        elementsToToggle.forEach(element => {
+            element.dataset.mode = newMode;
+        });
+        sessionStorage.setItem('theme', newMode);
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const closeBtn = document.getElementById('toggle-notif');
+	const body = document.getElementById('notification');
+	const elementsToToggle = [document.getElementById('notification')];
+	
+	const notifState = sessionStorage.getItem('notifState') || 'hide';
+
+	if (notifState === 'show'){
+		var locPhotos = sessionStorage.getItem("nbPhotosNotif");
+		var locExpo = sessionStorage.getItem("exposureTimeNotif");
+		var locTime = sessionStorage.getItem("currentTimeNotif");
+
+		notificationMessage.textContent = `${locPhotos} photos prises, exposition ${locExpo}.`;
+		notificationTitle.textContent = `La série se termine à ${locTime}`;
+	}
+
+
+	elementsToToggle.forEach(element => {
+		element.dataset.mode = notifState;
+	});
+	
+    closeBtn.addEventListener('click', () => {
+        const newMode = body.dataset.mode === 'show' ? 'hide' : 'show';
+        elementsToToggle.forEach(element => {
+            element.dataset.mode = newMode;
+        });
+        console.log(newMode);
+		sessionStorage.setItem('notifState', newMode);
+    });
+});
+
+function shutDown() { 
+    document.getElementById('main').style.display = "none";
+    document.getElementById('shutdown').style.display = "flex";
+	document.getElementById('shutdown').style.justifyContent = 'center';
+    sendPostRequest("shutdown");
+}
+
+function serverEnd(status_var) { 
+
+	const theme = sessionStorage.getItem('theme');
+	
+	const titleElement = document.querySelector('.shutdown-title');
+	const messageElement = document.querySelector('.shutdown-message');
+	const rotatingObject = document.querySelector('.rotating-object');
+	
+	if (theme === 'dark'){
+			// Update title
+			titleElement.textContent = "Hop au dodo";}
+			
+	if (theme === 'light'){
+			// Update title
+			titleElement.textContent = "Hop ça dégage";}
+			
+    document.getElementById('main').style.display = "none";
+    document.getElementById('shutdown').style.display = "flex";
+	document.getElementById('shutdown').style.justifyContent = 'center';
+	 
+    setTimeout(() => {
+		rotatingObject.style.animation = 'none';
+		if (theme === 'dark'){
+	        	// Update title
+	        	titleElement.textContent = "Bonne nuit";}
+			//messageElement.textContent = "L'intervallomètre est éteint";}
+		
+		if (theme === 'light'){
+	        	// Update title
+	        	titleElement.textContent = "Bonne journée";}
+		//messageElement.textContent = "L'intervallomètre est éteint";}
+		messageElement.textContent = "L'intervallomètre est éteint";
+        	rotatingObject.style.animation = 'none';
+		
+		titleElement.transition = 'opacity 0.7s ease-in-out';
+		messageElement.transition = 'opacity 0.7s ease-in-out';
+        //Optionally, redirect or perform other actions after a delay
+        // Stop rotating animation
+        rotatingObject.style.animation = 'none';
+        
+        // Smoothly hide rotating object
+        rotatingObject.style.transition = 'opacity 0.7s ease-in-out';
+        rotatingObject.style.opacity = '0';
+    }, 15000); // 15 seconds (15000 milliseconds)
+	sendPostRequest(status_var);
+	 
+}
+
+countdownInterval = setInterval(function() {
+	const timeHeader = new Date();
+	const hoursHeader = timeHeader.getHours().toString().padStart(2, '0');
+	const minutesHeader = timeHeader.getMinutes().toString().padStart(2, '0');
+	const currentTimeHeader = `${hoursHeader}:${minutesHeader}`;
+	const header = document.getElementById('left-side');
+	header.textContent = currentTimeHeader;
+	},)
