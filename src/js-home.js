186be70604db1d7c7fa49f537f5bcbe82bb432d5
Nicolas <69300323+Nicolas-M-Wong@users.noreply.github.com>
2024-08-10 11:44:03 +1100

    Update js-home.js

--- src/js-home.js
+++ src/js-home.js
@@ -359,13 +359,3 @@ countdownInterval = setInterval(function() {
 	const header = document.getElementById('left-side');
 	header.textContent = currentTimeHeader;
 	},)
-
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
-    .then(function(registration) {
-        console.log('Service Worker registered with scope:', registration.scope);
-    })
-    .catch(function(error) {
-        console.log('Service Worker registration failed:', error);
-    });
-}
