c30038131aa8de02b2265e362065ef3ad7bea435
Nicolas <69300323+Nicolas-M-Wong@users.noreply.github.com>
2024-08-14 09:39:40 +0200

    Update js-home.js

--- src/js-home.js
+++ src/js-home.js
@@ -148,7 +148,11 @@ function sendPostRequest(data) {
             if (xhr.status === 200) {
                 console.log('Success:', xhr.responseText);
                 if (data === 'battery'){
-                    update_battery(xhr.responseText)
+                    if (isNaN(xhr.responseText) === false){
+                        update_battery(xhr.responseText);
+                        } else {
+                        update_battery("");
+                        }
                 }
             } else {
                 console.error('Error:', xhr.statusText);
@@ -308,32 +312,31 @@ function serverEnd(status_var) {
 	
 	if (theme === 'dark'){
 			// Update title
-			titleElement.textContent = "Hop au dodo";}
-			
-	if (theme === 'light'){
-			// Update title
-			titleElement.textContent = "Hop ça dégage";}
-			
+            titleElement.textContent = "Hop au dodo";}
+
+    if (theme === 'light'){
+            // Update title
+            titleElement.textContent = "Hop ça dégage";}
+
     document.getElementById('main').style.display = "none";
     document.getElementById('shutdown').style.display = "flex";
-	document.getElementById('shutdown').style.justifyContent = 'center';
-	 
+    document.getElementById('shutdown').style.justifyContent = 'center';
+
     setTimeout(() => {
-		rotatingObject.style.animation = 'none';
-		if (theme === 'dark'){
-	        	// Update title
-	        	titleElement.textContent = "Bonne nuit";}
-			//messageElement.textContent = "L'intervallomètre est éteint";}
-		
-		if (theme === 'light'){
-	        	// Update title
-	        	titleElement.textContent = "Bonne journée";}
-		//messageElement.textContent = "L'intervallomètre est éteint";}
-		messageElement.textContent = "L'intervallomètre est éteint";
-        	rotatingObject.style.animation = 'none';
-		
-		titleElement.transition = 'opacity 0.7s ease-in-out';
-		messageElement.transition = 'opacity 0.7s ease-in-out';
+        rotatingObject.style.animation = 'none';
+        if (theme === 'dark'){
+                // Update title
+                titleElement.textContent = "Bonne nuit";}
+                //messageElement.textContent = "L'intervallomètre est éteint";}
+        if (theme === 'light'){
+            // Update title
+        titleElement.textContent = "Bonne journée";}
+        //messageElement.textContent = "L'intervallomètre est éteint";}
+        messageElement.textContent = "L'intervallomètre est éteint";
+            rotatingObject.style.animation = 'none';
+
+        titleElement.transition = 'opacity 0.7s ease-in-out';
+        messageElement.transition = 'opacity 0.7s ease-in-out';
         //Optionally, redirect or perform other actions after a delay
         // Stop rotating animation
         rotatingObject.style.animation = 'none';
@@ -342,29 +345,31 @@ function serverEnd(status_var) {
         rotatingObject.style.transition = 'opacity 0.7s ease-in-out';
         rotatingObject.style.opacity = '0';
     }, 15000); // 15 seconds (15000 milliseconds)
-	sendPostRequest(status_var);
-	 
+    sendPostRequest(status_var);
 }
 
 function changeColor(side) {
-	document.getElementById(`photo-distance-${side}`).style.backgroundColor = "#C70039"
-	setTimeout(() => {
-	document.getElementById(`photo-distance-${side}`).style.backgroundColor = "transparent";
-	}, 350); // Temps en millisecondes
+    document.getElementById(`photo-distance-${side}`).style.backgroundColor = "#C70039"
+    setTimeout(() => {
+    document.getElementById(`photo-distance-${side}`).style.backgroundColor = "transparent";
+    }, 350); // Temps en millisecondes
 }
 function update_time() {
-	const now = new Date();
-	const hoursHeader = now.getHours().toString().padStart(2, '0');
-	const minutesHeader = now.getMinutes().toString().padStart(2, '0');
-	const secondsHeader = now.getSeconds().toString().padStart(2, '0');
-	const currentTimeHeader = `${hoursHeader}:${minutesHeader}`;
-	const timerHeader = document.getElementById('timer-header');
-	timerHeader.textContent = currentTimeHeader;
+    const now = new Date();
+    const hoursHeader = now.getHours().toString().padStart(2, '0');
+    const minutesHeader = now.getMinutes().toString().padStart(2, '0');
+    const secondsHeader = now.getSeconds().toString().padStart(2, '0');
+    const currentTimeHeader = `${hoursHeader}:${minutesHeader}`;
+    const timerHeader = document.getElementById('timer-header');
+    timerHeader.textContent = currentTimeHeader;
 }
 
 function update_battery(batteyLevel) {
-	const batteryHeader = document.getElementById('battery-header');
-	batteryHeader.textContent = `${batteyLevel}%`;
+    const batteryHeader = document.getElementById('battery-header');
+    batteryHeader.textContent = `${batteyLevel}%`;
+    if (batteryLevel === ""){
+        sendPostRequest("battery");
+    }
 }
 function startUp() {
    update_time();
@@ -375,6 +380,3 @@ startUp();
 setInterval(function(){update_time();}, 1000)
 setInterval(function(){
     sendPostRequest("battery");},300000)
-
-
-   
