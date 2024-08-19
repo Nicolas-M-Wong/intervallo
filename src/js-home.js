ad048035d584d1ca23136ac033f8a610144946d9
Nicolas <69300323+Nicolas-M-Wong@users.noreply.github.com>
2024-08-19 11:26:29 +0200

    Update js-home.js

--- src/js-home.js
+++ src/js-home.js
@@ -661,7 +661,7 @@ function updating_values(pageName,currentPage) {
 		attachWheelEvents('enregistrement');
 		if (nbPhotosElement) {
 			
-			updateWheel("nb_photos", parseInt(sessionStorage.getItem("nb_photos_page_change"), 10), step_photo)
+			updateWheel("nb_photos", parseInt(sessionStorage.getItem("nb_photos_page_change"), 10), step_photo);
 		}
 	}
 }
