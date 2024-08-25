52dfff0894be2cd20db4fc6611562373ce1313c6
Nicolas <69300323+Nicolas-M-Wong@users.noreply.github.com>
2024-08-25 07:49:13 +0200

    Update js-home.js

--- src/js-home.js
+++ src/js-home.js
@@ -231,7 +231,7 @@ function sendPostRequest(data) {
                 if (xhr.status === 200) {
                     http_status_post = 200;
                     console.log('Success:', xhr.responseText);
-                    if (data === {...ensureDict("battery"), ...{"token":sessionStorage.getItem("sessionToken")) {
+                    if (data === {...ensureDict("battery"), ...{"token":sessionStorage.getItem("sessionToken")}) {
                         if (!isNaN(xhr.responseText)) {
                             updateBattery(xhr.responseText);
                         } else {
