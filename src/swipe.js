095455f9e2e968b151ea34345c4cca1da2bd9f50
Nicolas <69300323+Nicolas-M-Wong@users.noreply.github.com>
2024-09-08 03:17:25 +0200

    Create swipe.js

---
+++ src/swipe.js
@@ -0,0 +1,33 @@
+	// Variables to store touch positions
+	let startX, startY, endX, endY;
+
+	const minSwipeDistance = 0.5; 
+	// Function to handle the start of a touch event
+	function handleTouchStart(event) {
+		startX = event.touches[0].clientX;
+	}
+
+	// Function to handle the end of a touch event
+	function handleTouchEnd(event) {
+		const viewportWidth = window.innerWidth;
+		const minSwipeDistanceX = viewportWidth * minSwipeDistance;
+		endX = event.changedTouches[0].clientX;
+		const diffX = endX - startX;
+
+		if (Math.abs(diffX) > minSwipeDistanceX) {
+			// Horizontal swipe
+			if (diffX > 0) {
+				
+				changePage('home-V1');
+			} else {
+				
+				changePage('home-V1');
+			}
+		} 
+
+	}
+
+	// Attach touch event listeners to the swipe area
+	const swipeArea = document.getElementById('screen-container');
+	swipeArea.addEventListener('touchstart', handleTouchStart);
+	swipeArea.addEventListener('touchend', handleTouchEnd);
