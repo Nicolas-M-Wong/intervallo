dd52bfa1b866553cf2fa1a16f7164122d407b54e
Nicolas <69300323+Nicolas-M-Wong@users.noreply.github.com>
2024-09-21 09:48:34 +0200

    Update trigger_compiler.sh

--- trigger_compiler.sh
+++ trigger_compiler.sh
@@ -1,5 +1,11 @@
-echo "Running \"trigger_compiler.sh\""
+echo "Compiling trigger"
 
 g++ -Wall -o Trigger.exe Trigger.cpp -lpigpio -lrt
+
+echo "Compilation succeeded"
+
+echo "Compiling non constant exposure time"
+
+g++ -std=c++17 -o non-constant.exe non-constant.cpp
 
 echo "Compilation succeeded"
