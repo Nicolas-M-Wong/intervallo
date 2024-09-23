198dffad46aed292dda3625fbb1d1181d055f27b
Nicolas <69300323+Nicolas-M-Wong@users.noreply.github.com>
2024-09-24 00:42:13 +0200

    Update compiler.sh

--- compiler.sh
+++ compiler.sh
@@ -1,11 +1,11 @@
+if [[ "$1" == "Constant-Trigger" ]];then
 echo "Compiling trigger"
-
-g++ -Wall -o Trigger.exe Trigger.cpp -lpigpio -lrt
-
+g++ -Wall -o Constant-Trigger.exe Constant-Trigger.cpp -lpigpio -lrt
 echo "Compilation succeeded"
-
+fi
+if [[ "$1" == "Variable-Trigger" ]];then
 echo "Compiling non constant exposure time"
-
-g++ -std=c++17 -o non-constant.exe non-constant.cpp
-
+g++ -std=c++17 -o Variable-Trigger.exe Variable-Trigger.cpp
 echo "Compilation succeeded"
+fi
+
