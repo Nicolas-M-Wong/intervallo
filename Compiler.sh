561e93d70f69b69feefaaba37ce0aeedcf715eef
Nicolas <69300323+Nicolas-M-Wong@users.noreply.github.com>
2024-09-28 06:14:46 +0200

    Update Compiler.sh

--- Compiler.sh
+++ Compiler.sh
@@ -4,11 +4,11 @@ if [ "$1" = "Constant_Trigger" ]
     then
     echo "Compiling Constant_Trigger"
     g++ -Wall -o Constant_Trigger.exe Constant_Trigger.cpp -lpigpio -lrt
-    echo "Compilation succeeded"  
+    echo "Compilation $1 succeeded"  
 fi
 if [ "$1" = "Variable_Trigger" ]
     then
     echo "Compiling Variable_Trigger"
     g++ -std=c++17 -o Variable_Trigger.exe Variable_Trigger.cpp
-    echo "Compilation succeeded"
+    echo "Compilation $1 succeeded"
 fi
