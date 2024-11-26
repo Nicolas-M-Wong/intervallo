5acc4e510a7c01cb6360860e082123f06fe25c1d
Nicolas <69300323+Nicolas-M-Wong@users.noreply.github.com>
2024-11-26 17:41:50 +0100

    Update and rename trigger_compiler.sh to compiler.sh

---
+++ compiler.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ "$1" = "Constant_Trigger" ]
+    then
+    echo "Compiling Constant_Trigger"
+    g++ -Wall -o Constant_Trigger.exe Constant_Trigger.cpp -lpigpio -lrt
+    echo "Compilation $1 succeeded"  
+fi
+if [ "$1" = "Variable_Trigger" ]
+    then
+    echo "Compiling Variable_Trigger"
+    g++ -std=c++17 -o Variable_Trigger.exe Variable_Trigger.cpp
+    echo "Compilation $1 succeeded"
+fi
