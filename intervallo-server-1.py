93dd9ab63a904bb6606cfe699ca8dc6f3168f3cc
Nicolas <69300323+Nicolas-M-Wong@users.noreply.github.com>
2025-05-24 11:30:09 +0200

    Update intervallo-server-1.py

--- intervallo-server-1.py
+++ intervallo-server-1.py
@@ -244,17 +244,6 @@ def construct_vect (vect):
         output+=str(vect[i])+";"
     output+=str(vect[-1])+"]"
     return output
-
-def read_request(sock):
-    data = b''
-    while b'\r\n\r\n' not in data:
-        data += sock.recv(1024)
-    headers, body = data.split(b'\r\n\r\n', 1)
-    length = int(next((l.split(b':')[1].strip() for l in headers.split(b'\r\n') if l.lower().startswith(b'content-length')), b'0'))
-    while len(body) < length:
-        body += sock.recv(1024)
-    return headers.decode(), body.decode()
-
     
 time_delay = 0
 server_status = True
@@ -293,14 +282,13 @@ if TCP_IP != "127.0.0.1":
     while server_status:
 
         client_socket, client_address = server_socket.accept()
-        header_fct, body_received =  read_request(client_socket)
-        print(body_received)
         print(f'Accepted connection from {client_address}')
             # Receive the request data
         client_request = client_socket.recv(1024).decode('utf-8')
         # Parse the request to determine the type of request (GET/POST)
         headers = client_request.split('\r\n')
         expect_content_len = parse_header_item(headers,'Content-Length')
+        print(headers)
         # if expect_content_len != None:
         #     expect_content_len = int(expect_content_len)
         # if expect_content_len == None:
@@ -343,7 +331,7 @@ if TCP_IP != "127.0.0.1":
         
         elif method == 'POST':
             # Parse the request to extract form data
-            body = body_received
+            body = headers[-1]
             print(f"POST = {body}\n")
             response_body = "Data received"
             parameters = {}
